b28b61a87a8e78d945b3491aef579840a3d40220
Jaron Paige <jaronpaige@gmail.com>
2020-03-01 19:10:26 -0600

    Initial CSS styling, button 'active' state, etc

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const DataCtrl = (function() {
     const classesBtn = document.getElementById("getClasses");
     const racesBtn = document.getElementById("getRaces");
     const monstersBtn = document.getElementById("getMonsters");
+    const searchBar = document.getElementById("searchBar");
     const searchInput = document.getElementById("search-input");
 
-    async function fetchInfo(info) {
-        const res = await fetch(`http://www.dnd5eapi.co${info}`);
+    async function fetchInfo(url) {
+        const res = await fetch(`http://www.dnd5eapi.co${url}`);
         const data = await res.text();
         return data;
     }
@@ -18,6 +19,7 @@ const DataCtrl = (function() {
         racesBtn: racesBtn,
         monstersBtn: monstersBtn,
         fetchInfo: fetchInfo,
+        searchBar: searchBar,
         searchInput: searchInput
     };
 })();
@@ -41,72 +43,79 @@ const UICtrl = (function() {
         showRaceData: function(race) {
             moreInfoEl.innerHTML = "";
             moreInfoEl.innerHTML = `
-                <div><strong>Race:</strong> ${race.name}</div>
-                <div><strong>Ability Bonus:</strong> ${
-                    race.ability_bonuses.length > 0
-                        ? race.ability_bonuses.map(
-                              bonus => `
-                            <span>+${bonus.bonus} to <a href="#" class="tip" url="${bonus.url}" onmouseover="app.getToolTips(this, '${bonus.url}')">${bonus.name}</a></span>
-                        `
-                          )
-                        : "none"
-                }
-                <div><strong>Speed:</strong> ${race.speed}</div>
-                <div><strong>Alignment:</strong> ${race.alignment}</div>
-                <div><strong>Size:</strong> ${race.size_description}</div>
-                <div><strong>Starting Proficiencies:</strong> ${
-                    race.starting_proficiencies.length > 0
-                        ? race.starting_proficiencies.map(
-                              prof => `
-                                <span>${prof.name}</span>
-                        `
-                          )
-                        : "none"
+                <div class="card-title text-dark"><strong>Race:</strong> ${
+                    race.name
                 }</div>
-                <div><strong>Languages:</strong> ${race.language_desc}</div>
-                <div><strong>Traits:</strong> ${
-                    race.traits.length > 0
-                        ? race.traits.map(
-                              trait => `
-                        <span><a href="" class="tip" url="${trait.url}" onmouseover="app.getToolTips(this, '${trait.url}')">${trait.name}</a></span>
-                    `
-                          )
-                        : "none"
-                }</div>
-                <div><strong>Subraces:</strong> ${
-                    race.subraces.length > 0
-                        ? race.subraces.map(
-                              subrace => `
-                            <span>${subrace.name}</span>
+                <div class="card-text text-dark">
+                    <div><strong>Ability Bonus:</strong> ${
+                        race.ability_bonuses.length > 0
+                            ? race.ability_bonuses.map(
+                                  bonus => `
+                                <span>+${bonus.bonus} to <a href="#" class="tip" url="${bonus.url}" onmouseover="app.getToolTips(this, '${bonus.url}')">${bonus.name}</a></span>
+                            `
+                              )
+                            : "none"
+                    }
+                    <div><strong>Speed:</strong> ${race.speed}</div>
+                    <div><strong>Alignment:</strong> ${race.alignment}</div>
+                    <div><strong>Size:</strong> ${race.size_description}</div>
+                    <div><strong>Starting Proficiencies:</strong> ${
+                        race.starting_proficiencies.length > 0
+                            ? race.starting_proficiencies.map(
+                                  prof => `
+                                    <span>${prof.name}</span>
+                            `
+                              )
+                            : "none"
+                    }</div>
+                    <div><strong>Languages:</strong> ${race.language_desc}</div>
+                    <div><strong>Traits:</strong> ${
+                        race.traits.length > 0
+                            ? race.traits.map(
+                                  trait => `
+                            <span><a href="" class="tip" url="${trait.url}" onmouseover="app.getToolTips(this, '${trait.url}')">${trait.name}</a></span>
                         `
-                          )
-                        : "none"
-                }</div>
+                              )
+                            : "none"
+                    }</div>
+                    <div><strong>Subraces:</strong> ${
+                        race.subraces.length > 0
+                            ? race.subraces.map(
+                                  subrace => `
+                                <span>${subrace.name}</span>
+                            `
+                              )
+                            : "none"
+                    }</div>
+                </div>
             `;
         },
 
         showClassData: function(cls) {
             moreInfoEl.innerHTML = "";
             moreInfoEl.innerHTML = `
-                <div><strong>Class:</strong> ${cls.name}<div>
-                <div><strong>Hit Die:</strong> d${cls.hit_die}</div>
-                <div><strong>Proficiencies:</strong> ${cls.proficiencies.map(
-                    prof => `
-                    <span>${prof.name}</span>
-                `
-                )}</div>
-                <div><strong>Additional Proficiencies:</strong> Choose ${
-                    cls.proficiency_choices[0].choose
-                } - ${cls.proficiency_choices[0].from.map(
+                <div class="card-title text-dark"><strong>Class:</strong> ${
+                    cls.name
+                }<div>
+                <div class="card-text">
+                    <div><strong>Hit Die:</strong> d${cls.hit_die}</div>
+                    <div><strong>Proficiencies:</strong> ${cls.proficiencies.map(
+                        prof => `${prof.name}    
+                    `
+                    )}</div>
+                    <div><strong>Additional Proficiencies:</strong> Choose ${
+                        cls.proficiency_choices[0].choose
+                    } - ${cls.proficiency_choices[0].from.map(
                 prof => `
-                    <span>${prof.name}</span>
-                `
+                        <span>${prof.name.split(":")[1].trim()}</span>
+                    `
             )}</div>
-                <div><strong>Saving Throws:</strong> ${cls.saving_throws.map(
-                    save => `
-                    <a href="" onmouseover="app.getToolTips(this, '${save.url}')">${save.name}</a>
-                `
-                )}</div>
+                    <div><strong>Saving Throws:</strong> ${cls.saving_throws.map(
+                        save => `
+                        <a href="" onmouseover="app.getToolTips(this, '${save.url}')">${save.name}</a>
+                    `
+                    )}</div>
+                </div>
                 
                 
                 
@@ -116,26 +125,32 @@ const UICtrl = (function() {
         showSpellData: function(spell) {
             moreInfoEl.innerHTML = "";
             moreInfoEl.innerHTML = `
-                <div><strong>Name:</strong> ${spell.name}</div>
-                <div><strong>Level:</strong> ${spell.level}</div>
-                <div><strong>Classes:</strong> ${spell.classes.map(
-                    cla => `
-                    <span>${cla.name}</span>
-                `
-                )}</div>
-                <div><strong>Range:</strong> ${spell.range}</div>
-                <div><strong>Casting Time:</strong> ${spell.casting_time}</div>
-                <div><strong>Duration:</strong> ${spell.duration}</div>
-                <div><strong>Components:</strong> ${String(
-                    spell.components
-                )}</div>
-                <div><strong>Material:</strong> ${
-                    spell.material ? spell.material : "none"
-                }
-                <div><strong>Description:</strong> ${spell.desc}</div>
-                <div><strong>At Higher Levels:</strong> ${
-                    spell.higher_level ? spell.higher_level[0] : "n/a"
-                }
+                <div class="card-title text-dark"><strong>Name:</strong> ${
+                    spell.name
+                }</div>
+                <div class="card-text text-dark">
+                    <div><strong>Level:</strong> ${spell.level}</div>
+                    <div><strong>Classes:</strong> ${spell.classes.map(
+                        cla => `
+                        <span>${cla.name}</span>
+                    `
+                    )}</div>
+                    <div><strong>Range:</strong> ${spell.range}</div>
+                    <div><strong>Casting Time:</strong> ${
+                        spell.casting_time
+                    }</div>
+                    <div><strong>Duration:</strong> ${spell.duration}</div>
+                    <div><strong>Components:</strong> ${String(
+                        spell.components
+                    )}</div>
+                    <div><strong>Material:</strong> ${
+                        spell.material ? spell.material : "none"
+                    }
+                    <div><strong>Description:</strong> ${spell.desc}</div>
+                    <div><strong>At Higher Levels:</strong> ${
+                        spell.higher_level ? spell.higher_level[0] : "n/a"
+                    }
+                </div>
             `;
         },
 
@@ -144,16 +159,18 @@ const UICtrl = (function() {
             moreInfoEl.innerHTML = `
                 <div class="row">
                     <div class="col-6">
-                        <div><strong>Name:</strong> ${monster.name}</div>
-                        <div><strong>Size:</strong> ${monster.size}</div>
-                        <div><strong>Type:</strong> ${monster.type}</div>
-                        <div><strong>Alignment:</strong> ${monster.alignment}</div>
-                        <div><strong>AC:</strong> ${monster.armor_class}</div>
-                        <div><strong>Hit Points:</strong> ${monster.hit_points} (${monster.hit_dice})</div>
-                        <div><strong>CR:</strong> ${monster.challenge_rating}</div>
-                        <div><strong>Languages:</strong> ${monster.languages}</div>
+                        <div class="card-title text-dark"><strong>Name:</strong> ${monster.name}</div>
+                        <div class="card-text text-dark">
+                            <div><strong>Size:</strong> ${monster.size}</div>
+                            <div><strong>Type:</strong> ${monster.type}</div>
+                            <div><strong>Alignment:</strong> ${monster.alignment}</div>
+                            <div><strong>AC:</strong> ${monster.armor_class}</div>
+                            <div><strong>Hit Points:</strong> ${monster.hit_points} (${monster.hit_dice})</div>
+                            <div><strong>CR:</strong> ${monster.challenge_rating}</div>
+                            <div><strong>Languages:</strong> ${monster.languages}</div>
+                        </div>
                     </div>
-                    <div class="col-6">
+                    <div class="col-6 text-dark">
                         <div><strong>Stats:</strong>
                             <div class="row">
                                 <ul class="list-group col-6">
@@ -188,9 +205,24 @@ const app = (function(dataCtrl, ui) {
     // fetch info from api and display in list
     async function getInfo(e) {
         currentCategory = e.target.value;
+
         const info = await dataCtrl.fetchInfo(`/api/${currentCategory}`);
         parsedData = await JSON.parse(info).results;
         console.log(parsedData);
+
+        // on click, 'deselect' any active button
+        const mainButtons = document.querySelectorAll(".btn-main");
+        mainButtons.forEach(button => {
+            if (button.classList.contains("btn-danger")) {
+                button.classList.toggle("btn-danger");
+                button.classList.toggle("btn-secondary");
+            }
+        });
+
+        // give the clicked button the appearance of being active
+        e.target.classList.toggle("btn-secondary");
+        e.target.classList.toggle("btn-danger");
+
         ui.showData(parsedData);
     }
 
@@ -219,14 +251,45 @@ const app = (function(dataCtrl, ui) {
     async function showMoreData(e) {
         const info = await logData(e);
         if (currentCategory === "races") {
-            ui.showRaceData(JSON.parse(info));
+            const raceData = JSON.parse(info);
+            ui.showRaceData(raceData);
         } else if (currentCategory === "classes") {
+            const classData = JSON.parse(info);
             ui.showClassData(JSON.parse(info));
+            console.log(classData);
+            const equipment = await dataCtrl.fetchInfo(
+                classData.starting_equipment.url
+            );
+            const equipmentData = JSON.parse(equipment);
         } else if (currentCategory === "spells") {
-            ui.showSpellData(JSON.parse(info));
+            const spellData = JSON.parse(info);
+            ui.showSpellData(spellData);
         } else if (currentCategory === "monsters") {
-            ui.showMonsterData(JSON.parse(info));
+            const monsterData = JSON.parse(info);
+            ui.showMonsterData(monsterData);
         }
+    }
+
+    async function getToolTips(el, url) {
+        const tip = await dataCtrl.fetchInfo(url);
+        const tipJSON = JSON.parse(tip);
+        let tipDesc = "";
+        await tipJSON.desc.forEach(desc => (tipDesc += desc));
+        // console.log(tipDesc);
+        el.setAttribute("data-toggle", "tooltip");
+        el.setAttribute("data-placement", "top");
+        el.setAttribute("data-original-title", tipDesc);
+
+        // show tooltip
+        $(function() {
+            $(el).tooltip("show");
+        });
+
+        // overwrite default tooltip width
+        $(".tip").tooltip({
+            template:
+                '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner large"></div></div>'
+        });
     }
 
     // event listeners
@@ -236,20 +299,6 @@ const app = (function(dataCtrl, ui) {
     dataCtrl.monstersBtn.addEventListener("click", getInfo);
     dataCtrl.searchInput.addEventListener("input", filterList);
     ui.outputEl.addEventListener("click", showMoreData);
-
-    async function getToolTips(el, url) {
-        const tip = await dataCtrl.fetchInfo(url);
-        const tipJSON = JSON.parse(tip);
-        let tipDesc = "";
-        await tipJSON.desc.forEach(desc => (tipDesc += desc));
-        console.log(tipDesc);
-        el.setAttribute("data-toggle", "tooltip");
-        el.setAttribute("data-placement", "top");
-        el.setAttribute("data-original-title", tipDesc);
-        $(function() {
-            $(el).tooltip("show");
-        });
-    }
 
     return {
         getToolTips: getToolTips
